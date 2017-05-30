64d28be38e5aa9bdc830fb555db3594d8c023c18
AciD <xlxAciDxlx@users.noreply.github.com>
2017-05-30 17:40:35 -0400

    v1.2.0
    - Refactored a majority if the switcher JavaScript
    - Added the ability to simply select a file that has your topics in it
    rather than having to edit JavaScript
    - Added better Getting Started instructions
    - Added a Feedback section to `README.md`
    - Added a section to `README.md` explaining how to capture the topic
    list in OBS. XSplit instructions will be added eventually
    - Moved CSS into it's own file, rather than in `index.html`
    - Moved JS and CSS into their own assets directory

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Topic-Switcher
-A simple Topic Switching app, great for podcasts and discussion video/livestreams
+A simple Topic Switching app, great for podcasts and discussion videos/livestreams
 
 ### Getting Started
 - Download the latest archive by [clicking here](https://github.com/xlxAciDxlx/Topic-Switcher/archive/master.zip)
-- Unzip it using your preferred Zip Manager
-- Open *js/switcher.js* in your text editor of choice (I recommend [Notepad++]()) and change the list to include your topics.
+- Unzip it using your preferred archive manager (Win-Rar, 7Zip, etc)
+- Open `topics.txt` in your favorite text editor (I recommend [Notepad++](https://notepad-plus-plus.org/)) and add your topics, one topic per line
+- Save any edited files
+- Right click on `index.html` and open in your web browser of choice
+- *Optional:* If you'd like to customize the look and feel, open up `assets/css/style.css`
+
+#### Capturing with OBS
+- To capture your topic list with OBS, simply add a Browser Source and copy the URL that your browser took you to when you opened `index.html`
+
+### Feedback/Feature Suggestions
+If you have any feedback or feature suggestions, head over to the "Issues" tab and post it there.
---
+++ assets/js/switcher.js
@@ -0,0 +1,114 @@
+var file = "";
+var hideOtherTopics = 0;
+var lastShownTopic = 0;
+var maxTopics = 4;
+var topics = [];
+var topicContainer = document.getElementById("topics");
+
+function getVisibleTopics() {
+        var topicsToCheck = document.getElementsByTagName("li");
+        var visibleTopics = 0;
+
+        for (i = 0; i < topicsToCheck.length; i++) {
+                if (topicsToCheck[i].className != "hidden") {
+                        visibleTopics++;
+                }
+        }
+
+        return visibleTopics;
+}
+
+function manageTopics(start) {
+        if (topics.length > 0) {
+                if (start > 0) {
+                        start = (+start + 1);
+                        console.info("Showing topics starting at " + start);
+                }
+
+                var visibleTopics = getVisibleTopics();
+                if (visibleTopics == maxTopics) {
+                        return;
+                }
+
+                if (visibleTopics == 0 && lastShownTopic == topics.length) {
+                        document.getElementById("fileSelector").setAttribute("class", "fileSelector hidden");
+                }
+
+                var maxTopicsLoop = maxTopics;
+                if (visibleTopics > 0) {
+                        maxTopicsLoop = (maxTopics - visibleTopics);
+                }
+
+                for (i = start; i < topics.length && i < (start + maxTopicsLoop); i++) {
+                        var topicItem = document.createElement("li");
+                        topicItem.setAttribute("class", "topic");
+                        topicItem.setAttribute("id", "topic" + i);
+                        topicItem.appendChild(document.createTextNode(topics[i]));
+
+                        if (i == 0) {
+                                topicItem.setAttribute("class", "topic current");
+                        }
+
+                        topicContainer.appendChild(topicItem);
+                        lastShownTopic = i;
+                }
+        }
+}
+
+function readFile() {
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function(e) {
+                document.getElementById("noTopics").setAttribute("class", "hidden");
+
+                topics = reader.result.split("\n");
+                manageTopics(lastShownTopic);
+        }
+}
+
+window.onload = function() {
+        var fileInput = document.getElementById("fileInput");
+        fileInput.addEventListener("change", function(e) {
+                file = fileInput.files[0];
+                readFile();
+
+                document.getElementById("fileSelector").setAttribute("class", "fileSelector hidden");
+        });
+}
+
+window.setInterval(function() {
+        readFile();
+}, 5000);
+
+topicContainer.addEventListener("click", function(e) {
+        if (e.target && e.target.nodeName == "LI") {
+                var originalId = e.target.id;
+                var id = originalId.replace(/topic/, "");
+                var item = document.getElementById(originalId);
+
+                if (hideOtherTopics && item.className != "topic current") {
+                        return;
+                }
+
+                if (hideOtherTopics) {
+                        item.className = "hidden";
+                } else {
+                        item.className = "topic";
+                }
+
+                var nextId = +id + 1;
+                var nextElement = document.getElementById("topic" + nextId);
+                if (nextElement) {
+                        nextElement.setAttribute("class", "topic current");
+                }
+
+                var visibleTopics = getVisibleTopics();
+                if (topics.length == 0 || visibleTopics == maxTopics) {
+                        return;
+                }
+
+                if (lastShownTopic > 0 && lastShownTopic > 0 && lastShownTopic != topics.length) {
+                        manageTopics(lastShownTopic);
+                }
+        }
+});
